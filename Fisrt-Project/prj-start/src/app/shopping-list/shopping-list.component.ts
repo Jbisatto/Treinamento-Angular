9fa921fe2844c2355ca121afdbd8dad9013e43a5
Jefferson Bisatto <jefferson.bisatto@gmail.com>
2023-07-20 12:06:55 -0300

    Game par/impar output/input

--- Fisrt-Project/prj-start/src/app/shopping-list/shopping-list.component.ts
+++ Fisrt-Project/prj-start/src/app/shopping-list/shopping-list.component.ts
@@ -7,7 +7,10 @@ import { Ingredients } from '../shared/ingredient.models';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredients[] = [];
+  ingredients: Ingredients[] = [
+    new Ingredients('Aplles', 5),
+    new Ingredients('Tomatoes', 10)
+  ];
   constructor() { }
 
   ngOnInit(): void {
